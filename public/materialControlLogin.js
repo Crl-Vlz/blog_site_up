6597b67c345f139367b4ca325d85a33931da0b7e
Vélez <73377541+Crl-Vlz@users.noreply.github.com>
2021-10-26 00:43:37 -0500

    Added js Control for login page

---
+++ public/materialControlLogin.js
@@ -0,0 +1,9 @@
+const MDCTextField = mdc.textField.MDCTextField;
+
+const MDCTextFieldFoundation = mdc.textField.MDCTextFieldFoundation;
+
+// Text Field for Login UI
+
+const email = new MDCTextField(document.querySelector(".mail"));
+const username = new MDCTextField(document.querySelector(".username"));
+const password = new MDCTextField(document.querySelector(".password"));
