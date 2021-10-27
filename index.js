de566ed2d2ceb239cbe97827532cfb73b7d9079f
Vélez <73377541+Crl-Vlz@users.noreply.github.com>
2021-10-26 23:43:06 -0500

    Shows post even when changing posts

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const express = require("express");
 const fs = require("fs");
 //const bodyParser = require("body-parser");
 const cheerio = require("cheerio");
+const { title } = require("process");
 const $ = cheerio.load(`<!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="UTF-8" />
@@ -180,98 +182,6 @@ const $ = cheerio.load(`<!DOCTYPE html>
         </div>
         <div class="mdc-dialog__scrim"></div>
       </div>
-
-      <!--PopUp EDIT-->
-      <!--When opened add class mdc-dialog--open from JS-->
-      
-      <div class="mdc-dialog myPopUp" id="popup2">
-        <div class="mdc-dialog__container">
-          <div
-            class="mdc-dialog__surface"
-            role="dialog"
-            aria-modal="true"
-            aria-labelledby="my-dialog-title"
-            aria-describedby="my-dialog-content"
-          >
-            <div class="mdc-dialog__header Head">
-              <h2 class="mdc-dialog__title T" id="my-dialog-title">
-                Edit my Post
-              </h2>
-              <button
-                class="mdc-icon-button material-icons mdc-dialog__close C"
-                data-mdc-dialog-action="close"
-                id="closeEdit"
-              >
-                close
-              </button>
-            </div>
-            <form action="/editPost" method="POST">
-              <div class="mdc-dialog__content myForm" id="my-dialog-content">
-                <!--FORMS-->
-                <br>
-                <div>
-                  <label class="mdc-text-field mdc-text-field--outlined myTitle">
-                    <span class="mdc-notched-outline">
-                      <span class="mdc-notched-outline__leading"></span>
-                      <span class="mdc-notched-outline__trailing"></span>
-                    </span>
-                    <input type="text" class="mdc-text-field__input" name="title" value="Title">
-                  </label>
-                </div>
-                <br>
-                <label class="mdc-text-field mdc-text-field--textarea myTA">
-                  <span class="mdc-notched-outline myTA">
-                    <span class="mdc-notched-outline__leading"></span>
-                    <span class="mdc-notched-outline__trailing"></span>
-                  </span>
-                  <span class="mdc-text-field__resizer">
-                    <textarea
-                    class="mdc-text-field__input"
-                    aria-labelledby="my-label-id"
-                    name="description"
-                    rows="8"
-                    cols="40"
-                    maxlength="140"
-                    ></textarea>
-                  </span>
-                </label>
-                <br />
-                <br />
-                <label class="mdc-text-field mdc-text-field--filled imageURL">
-                  <span class="mdc-text-field__ripple"></span>
-                  <span class="mdc-floating-label" id="my-label-id"
-                  >Image URL</span
-                  >
-                  <input
-                  class="mdc-text-field__input txtInput"
-                  type="text"
-                  name="url"
-                  aria-labelledby="my-label-id"
-                  />
-                  <span class="mdc-line-ripple"></span>
-                </label>
-                <div class="mdc-text-field-helper-line">
-                  <div class="mdc-text-field-character-counter">0 / 140</div>
-                </div>
-              </div>
-              <div class="mdc-dialog__actions">
-                <button
-                type="submit"
-                name="submit"
-                class="mdc-button mdc-dialog__button"
-                data-mdc-dialog-action="ok"
-                id="postEdited"
-                >
-                <div class="mdc-button__ripple"></div>
-                <span class="mdc-button__label Ok">Save Changes</span>
-              </button>
-            </form>
-            </div>
-          </div>
-        </div>
-        <div class="mdc-dialog__scrim"></div>
-      </div>
-
     </main>
 
     <!--Personal CSS-->
@@ -408,7 +318,9 @@ app
 app
   .route("/posts")
   .get((req, res) => {
-    res.send(generalPosts);
+    if (generalPosts.length - 1) {
+      res.send($.html());
+    }
   })
   .post((req, res) => {
     let userId = activeUser.id; // true
@@ -419,61 +331,92 @@ app
 
     /*  if(userId == undefined){
         res.send("Invalid User ID");
-    }else */if(title == null){
-        res.send("Invalid Title");
-    }else if(description == null){
-        res.send("Invalid Description");
-    }
-    else{
-        let x = {id: newId, userId: userId, title: title, description: description, imageUrl: url}
-        generalPosts.push(x);
-        $('<div class="mdc-card mdc-card-outlined card mainBody" id="'
-        +String(x.id)+'"> <div class="post-title"><h1 class="cardTitle" id="'
-        +String(x.id)+'">Post Title</h1></div> <img id="'
-        +String(x.id)+'" class="my-card__media mdc-card__media mdc-card__media--16-9 myCard" src=""> <div class="cardText" id="'
-        +String(x.id)+'">Sample Text YEEEEE</div><div class="bottom-drawer"><form class="mdc-button mdc-button--icon__leading post-btn" action="/deletePost" method="POST"> <input type="text" id='+String(x.id)+' class="mdc-button mdc-button--icon__leading post-btn mdc-text-field__input myPostID" style="position: absolute;" name="postID" value=""><button aria-label="View Post" class="mdc-button mdc-button--icon__leading post-btn type="submit"><span class="mdc-button__ripple delPost"'+String(x.id)+'"></span><i class="material-icons mdc-button__icon" aria-hidden="true">delete</i><span class="mdc-button__label">Delete Post</span></button></form><button aria-label="View Post" class="mdc-button mdc-button--icon__leading post-btn editPopUp" id="editPost"><span class="mdc-button__ripple"></span><i class="material-icons mdc-button__icon" aria-hidden="true">edit</i> <span class="mdc-button__label">Edit Post</span> </button> </div> </div><br>').appendTo("#postingArea")
-        
-        $("#"+String(x.id)+".cardTitle").text(x.title);
-        $("#"+String(x.id)+".cardText").text(x.description);
-        $("#"+String(x.id)+".myCard").attr("src", x.imageUrl);
-        $("#"+String(x.id)+".myPostID").attr("value", x.id);
+    }else */ if (title == null) {
+      res.send("Invalid Title");
+    } else if (description == null) {
+      res.send("Invalid Description");
+    } else {
+      let x = {
+        id: newId,
+        userId: userId,
+        title: title,
+        description: description,
+        imageUrl: url,
+      };
+      generalPosts.push(x);
+      $(
+        '<div class="mdc-card mdc-card-outlined card mainBody" id="' +
+          String(x.id) +
+          '"> <div class="post-title"><h1 class="cardTitle" id="' +
+          String(x.id) +
+          '">Post Title</h1></div> <img id="' +
+          String(x.id) +
+          '" class="my-card__media mdc-card__media mdc-card__media--16-9 myCard" src=""> <div class="cardText" id="' +
+          String(x.id) +
+          '">Sample Text YEEEEE</div><div class="bottom-drawer"><form class="mdc-button mdc-button--icon__leading post-btn" action="/deletePost" method="POST"> <input type="text" id=' +
+          String(x.id) +
+          ' class="mdc-button mdc-button--icon__leading post-btn mdc-text-field__input myPostID" style="position: absolute;" name="postID" value=""><button aria-label="View Post" class="mdc-button mdc-button--icon__leading post-btn type="submit"><span class="mdc-button__ripple delPost"' +
+          String(x.id) +
+          '"></span><i class="material-icons mdc-button__icon" aria-hidden="true">delete</i><span class="mdc-button__label">Delete Post</span></button></form><button aria-label="View Post" class="mdc-button mdc-button--icon__leading post-btn" id="editPost"><span class="mdc-button__ripple"></span><i class="material-icons mdc-button__icon" aria-hidden="true">edit</i> <span class="mdc-button__label">Edit Post</span> </button> </div> </div><br>'
+      ).appendTo("#postingArea");
 
-        res.send($.html());
+      $("#" + String(x.id) + ".cardTitle").text(x.title);
+      $("#" + String(x.id) + ".cardText").text(x.description);
+      $("#" + String(x.id) + ".myCard").attr("src", x.imageUrl);
+      $("#" + String(x.id) + ".myPostID").attr("value", x.id);
+
+      res.send($.html());
     }
-})
-  .put( (req, res) => {
+  })
+  .put((req, res) => {
     let id = req.body.id;
     let title = req.body.title;
     let description = req.body.description;
     let url = req.body.imageUrl;
     let index;
 
-    for(let i = 0; i < generalPosts.length; i++){
-        if(generalPosts[i].id == id){
-            index = i;
-            break;
-        }
+    for (let i = 0; i < generalPosts.length; i++) {
+      if (generalPosts[i].id == id) {
+        index = i;
+        break;
+      }
     }
 
-    if(index == null){
-        res.send("Error: invalid ID");
+    if (index == null) {
+      res.send("Error: invalid ID");
+    } else if (title == null) {
+      res.send("Error: invalid title");
+    } else if (description == null) {
+      res.send("Error: invalid description");
+    } else {
+      generalPosts[index].title = title;
+      generalPosts[index].description = description;
+      generalPosts[index].imageUrl = url;
+      $("#" + String(generalPosts[index].id) + ".cardTitle").text(
+        generalPosts[index].title
+      );
+      $("#" + String(generalPosts[index].id) + ".cardText").text(
+        generalPosts[index].description
+      );
+      $("#" + String(generalPosts[index].id) + ".myCard").attr(
+        "src",
+        generalPosts[index].imageUrl
+      );
+      $("#" + String(x.id) + ".card.mainBody").add;
+      res.send($.html());
     }
-    else if(title == null){
-        res.send("Error: invalid title");
-    }
-    else if(description == null){
-        res.send("Error: invalid description");
-    }else{
-        generalPosts[index].title = title;
-        generalPosts[index].description = description;
-        generalPosts[index].imageUrl = url;
-        $("#"+String(generalPosts[index].id)+".cardTitle").text(generalPosts[index].title);
-        $("#"+String(generalPosts[index].id)+".cardText").text(generalPosts[index].description);
-        $("#"+String(generalPosts[index].id)+".myCard").attr("src", generalPosts[index].imageUrl);
-        $("#"+String(x.id)+".card.mainBody").add; 
-        res.send($.html());
+  })
+  .delete((req, res) => {
+    let id = req.body.$(".card.mainBody").id;
+
+    for (let i = 0; i < generalPosts.length; i++) {
+      if (generalPosts[i].id == id) {
+        $("#" + String(x.id) + ".card.mainBody").remove();
+        generalPosts.pop(generalPosts[i]);
+      }
     }
 
+    res.send($.html());
   });
 
 app
@@ -495,51 +438,47 @@ app
     activeUser.user = newUser;
   });
 
-  app.route("/deletePost")
-  .post((req, res) => {
-    let id = req.body.postID;
+app.route("/deletePost").post((req, res) => {
+  let id = req.body.postID;
 
-    for(let i = 0; i < generalPosts.length; i++){
-        if(generalPosts[i].id == id){
-            $("#"+String(generalPosts[i].id)+".card.mainBody").remove();
-            generalPosts.pop(generalPosts[i]);
-        }
+  for (let i = 0; i < generalPosts.length; i++) {
+    if (generalPosts[i].id == id) {
+      $("#" + String(generalPosts[i].id) + ".card.mainBody").remove();
+      generalPosts.pop(generalPosts[i]);
     }
-    res.send($.html());
-  })
+  }
+  res.send($.html());
+});
 
-  app.route("/postToEdit")
-  .post((req,res) =>{
-    global.currentID = req.body.postID;
-  })
+app.route("/editPost").post((req, res) => {
+  console.log("HBJHNKJ", req.body.postID);
+  let index = req.body.postID;
 
-  app.route("/editPost")
-  .post((req, res) => {
-        let title = req.body.title; // true
-        let description = req.body.description; // true
-        let url = req.body.url;
-        let index = generalPosts
-        console.log(index)
+  generalPosts[index].title = title;
+  generalPosts[index].description = description;
+  generalPosts[index].imageUrl = url;
+  $("#" + String(generalPosts[index].id) + ".cardTitle").text(
+    generalPosts[index].title
+  );
+  $("#" + String(generalPosts[index].id) + ".cardText").text(
+    generalPosts[index].description
+  );
+  $("#" + String(generalPosts[index].id) + ".myCard").attr(
+    "src",
+    generalPosts[index].imageUrl
+  );
+  $("#" + String(x.id) + ".card.mainBody").add;
 
-            /*  if(userId == undefined){
-        res.send("Invalid User ID");
-    }else */if(title == null){
-        res.send("Invalid Title");
-    }else if(description == null){
-        res.send("Invalid Description");
+  var postId = -1;
+
+  generalPosts.forEach((element) => {
+    if (element.title === title) {
+      postId = element.id;
     }
-    else{
-        generalPosts[index].title = title;
-        generalPosts[index].description = description;
-        generalPosts[index].imageUrl = url;
-        $("#"+String(generalPosts[index].id)+".cardTitle").text(generalPosts[index].title);
-        $("#"+String(generalPosts[index].id)+".cardText").text(generalPosts[index].description);
-        $("#"+String(generalPosts[index].id)+".myCard").attr("src", generalPosts[index].imageUrl);
-        $("#"+String(generalPosts[index].id)+".card.mainBody").add; 
-    }
-        res.send($.html());
+  });
 
-  })
+  res.send($.html());
+});
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
--- public/UIcontrol.js
+++ public/UIcontrol.js
@@ -1,24 +1,30 @@
-$("#btn-add").click(function(){
-    $("#popup").addClass("mdc-dialog--open");
+$("#btn-add").click(function () {
+  $("#popup").addClass("mdc-dialog--open");
 });
 
-$(".editPopUp").click(function(){
-    $("#popup2").addClass("mdc-dialog--open");
-    
+$(".editPopUp").click(function () {
+  $("#popup2").addClass("mdc-dialog--open");
 });
 
-$("#postOK").click(function(){
-    $("#popup").removeClass("mdc-dialog--open");
+$("#postOK").click(function () {
+  $("#popup").removeClass("mdc-dialog--open");
 });
 
-$("#postEdited").click(function(){
-    $("#popup2").removeClass("mdc-dialog--open");
+$("#postEdited").click(function () {
+  $("#popup2").removeClass("mdc-dialog--open");
 });
 
-$("#closePost").click(function(){
-    $("#popup").removeClass("mdc-dialog--open");
+$("#closePost").click(function () {
+  $("#popup").removeClass("mdc-dialog--open");
 });
 
-$("#closeEdit").click(function(){
-    $("#popup2").removeClass("mdc-dialog--open");
+$("#closeEdit").click(function () {
+  $("#popup2").removeClass("mdc-dialog--open");
 });
+
+$(document).ready(
+  $(function () {
+    $("#showPosts").click();
+    console.log("Was clicked");
+  })
+);
